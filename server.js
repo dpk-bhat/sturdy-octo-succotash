d4356d0487f4340ffe23ce19bc955aabf0d4ffb6
H Deepak D Bhat <h.deepak.d.bhat@sap.com>
2023-07-19 20:35:49 +0530

    added middleware for error handling

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
 // library to load the .env file to process.env
 const dotEnv = require("dotenv").config();
 // An instance of Express
@@ -8,8 +9,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // app.use is used for setting the middleware.
+
+// express.json() is the middleware used for parsing the body to json format
+app.use(express.json());
+
 // here using this overloaded method: for '/api/contacts' path, use this router exported by "./routes/contactRoutes"
 app.use('/api/contacts', require("./routes/contactRoutes"));
+
+// adding a errorHandler as a middleware to parse and present the error message
+app.use(errorHandler);
 
 // app.listen is used for creating the server
 // here using this implementation to listen to port (from .env or 5000)
